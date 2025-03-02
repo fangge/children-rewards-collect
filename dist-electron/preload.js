f0bcb289d36738ee3c088a39742f0837ff2655db
MrFanggesun <mickshifangge@gmail.com>
2025-03-02 14:31:22 +0800

    迭代

--- dist-electron/preload.js
+++ dist-electron/preload.js
@@ -1,19 +1,19 @@
-const { contextBridge, ipcRenderer } = require("electron");
-contextBridge.exposeInMainWorld("electronAPI", {
+const { contextBridge: i, ipcRenderer: e } = require("electron");
+i.exposeInMainWorld("electronAPI", {
   // 孩子信息管理
-  saveChildren: (children) => ipcRenderer.invoke("save-children", children),
-  getChildren: () => ipcRenderer.invoke("get-children"),
+  saveChildren: (a) => e.invoke("save-children", a),
+  getChildren: () => e.invoke("get-children"),
   // 奖项管理
-  saveRewards: (rewards) => ipcRenderer.invoke("save-rewards", rewards),
-  getRewards: () => ipcRenderer.invoke("get-rewards"),
+  saveRewards: (a) => e.invoke("save-rewards", a),
+  getRewards: () => e.invoke("get-rewards"),
   // 图片管理
-  saveImage: (imageData, fileName) => ipcRenderer.invoke("save-image", { imageData, fileName }),
-  selectImage: () => ipcRenderer.invoke("select-image"),
+  saveImage: (a, t) => e.invoke("save-image", { imageData: a, fileName: t }),
+  selectImage: () => e.invoke("select-image"),
   // 数据目录管理
-  setDataDir: (path) => ipcRenderer.invoke("set-data-dir", path),
-  getDataDir: () => ipcRenderer.invoke("get-data-dir"),
-  selectDataDir: () => ipcRenderer.invoke("select-data-dir"),
+  setDataDir: (a) => e.invoke("set-data-dir", a),
+  getDataDir: () => e.invoke("get-data-dir"),
+  selectDataDir: () => e.invoke("select-data-dir"),
   // 数据导入导出
-  exportData: () => ipcRenderer.invoke("export-data"),
-  importData: () => ipcRenderer.invoke("import-data")
+  exportData: () => e.invoke("export-data"),
+  importData: () => e.invoke("import-data")
 });
